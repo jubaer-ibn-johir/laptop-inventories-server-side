0b7de0b4b64a1056e23a70102e5b3068ecb7a8a3
jubaer ibn zohir <jubaeribnzohir@gmail.com>
2022-05-05 02:55:38 +0600

    id added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,36 @@ const app = express();
 //middleware 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cck3g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority` ;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+    try{
+        await client.connect();
+        const laptopCollection = client.db('laptops').collection('inventories');
+
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = laptopCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        });
+
+        app.get('/inventory/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const inventory = await laptopCollection.findOne(query);
+            res.send(inventory);
+        })
+    }
+    finally{
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Running Laptop Inventories Server');
