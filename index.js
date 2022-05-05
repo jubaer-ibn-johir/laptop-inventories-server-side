48b43e3858699057d153108b3c8bbcf14cad1e63
jubaer ibn zohir <jubaeribnzohir@gmail.com>
2022-05-05 19:05:18 +0600

    delete confirm

--- index.js
+++ index.js
@@ -39,6 +39,14 @@ async function run (){
             const newItem = req.body;
             const result = await laptopCollection.insertOne(newItem);
             res.send(result);
+        });
+
+        // DELETE 
+        app.delete('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await laptopCollection.deleteOne(query);
+            res.send(result);
         })
 
 
