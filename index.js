302b57ed8b933a53bad4cc252520b7aae5cc84c5
jubaer ibn zohir <jubaeribnzohir@gmail.com>
2022-05-08 22:28:14 +0600

    heroku added

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cck3g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority` ;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+// AUTH 
+// app.post('/login', async (req, res) => {
+//     const user = req.body;
+//     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+//         expiresIn: '1d'
+//     });
+//     res.send({accessToken});
+// })
+
 async function run (){
     try{
         await client.connect();
